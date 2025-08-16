9f94c204a694b05a430d46b0587281d5a7d87f11
Dan Ciobanu <c.dan79@yahoo.com>
2025-08-16 12:07:00 +0300

    feat:add openApi

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  writeFileSync("./swagger-spec.json", JSON.stringify(documentFactory()));
+
   SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
